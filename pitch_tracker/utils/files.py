713dd7c82cffc814efd2ccd4bfc0ba1e8ed206a8
duotien <notduotien@gmail.com>
2023-02-09 20:04:31 +0700

    add utils to save&load pickle

---
+++ pitch_tracker/utils/files.py
@@ -0,0 +1,101 @@
+import pickle
+from pathlib import Path
+from datetime import datetime
+
+def save_pickle(obj, file_path, dir_path=None, is_overwrite=True):
+    """Save the object to a pickled file, currently using pickle protocol 5
+
+    Parameters
+    ----------
+    obj: Python object
+        the object to save to file
+
+    file_path : str
+        the path to the file (with filename) to save this obj to
+
+    dirpath: str
+        the path to the directory to save this obj to, the file name of the obj is `obj_ddmmyyyy_hhmmss.pkl`
+        with ddmmyyyy_hhmmss is the current datetime when saving the obj
+
+    is_overwrite: bool
+        whether to overwrite the obj to this file_path, if exists
+        if not overwrite the file, the file name of the obj to save is `obj_ddmmyyyy_hhmmss.pkl`
+        with ddmmyyyy_hhmmss is the current datetime when saving the obj
+
+    Returns
+    -------
+    pathlib.Path
+        the Path instance of the saved object, None if cannot save
+    """
+    if obj is None:
+        return None
+    if not file_path:
+        if not dir_path:
+            return None
+        elif not isinstance(dir_path, str):
+            raise TypeError(f'dir_path is a str, not {type(dir_path).__name__}')
+    elif not isinstance(file_path, str):
+        raise TypeError(f'file_path is a str, not {type(file_path).__name__}')
+
+    protocol = pickle.HIGHEST_PROTOCOL
+    obj_file = Path(file_path)
+
+    if obj_file.is_file():
+        if is_overwrite:
+            with obj_file.open(mode='wb') as f:
+                pickle.dump(obj, f, protocol=protocol)
+            return obj_file
+        else:
+            obj_filename = obj_file.stem + '_' + datetime.now().strftime('%d%m%Y_%H%M%S') + \
+                obj_file.suffix
+            modified_obj_file = obj_file.with_name(obj_filename)
+            with modified_obj_file.open(mode='wb') as f:
+                pickle.dump(obj, f, protocol=protocol)
+            return modified_obj_file
+    # file_path not exists, get the valid parent directory,
+    # or check if file_path is a directory already or use the dir_path
+    obj_dir = obj_file if obj_file.is_dir() else (
+        obj_file.parent if obj_file.parent.is_dir() else Path(dir_path)
+    )
+    # get the name of file_path to be used as filename
+    obj_filename = obj_file.name if not obj_file.is_dir() else ''
+    if obj_dir.is_dir():
+        obj_filename = ('obj_' + datetime.now().strftime('%d%m%Y_%H%M%S') +
+                        '.pkl' if not obj_filename else obj_filename)
+        obj_file = obj_dir.joinpath(obj_filename)
+        with obj_file.open(mode='wb') as f:
+            pickle.dump(obj, f, protocol=protocol)
+        return obj_file
+    return None
+
+def load_pickle(file_path):
+    """Save an object from a pickled file
+
+    Parameters
+    ----------
+    file_path : str
+        the path to the file to load this obj to
+
+    Returns
+    -------
+    object or None
+        the loaded object or None if cannot load
+    """
+    if file_path is None:
+        return None
+    elif not isinstance(file_path, str):
+        raise TypeError(f'file_path is a str, not {type(file_path).__name__}')
+
+    obj_file = Path(file_path)
+
+    if obj_file.is_file():
+        # try:
+        with obj_file.open(mode='rb') as f:
+            obj = pickle.load(f)
+            return obj
+        # except ValueError:
+        #     import pickle5
+        #     with obj_file.open(mode='rb') as f:
+        #         obj = pickle5.load(f)
+        #         return obj
+    return None
