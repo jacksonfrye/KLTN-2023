eb2bb756831dd7fe3d9a9a4ceebd5cbcfeb34e62
duotien <notduotien@gmail.com>
2023-04-05 13:57:05 +0700

    account for batch/non-batch

--- pitch_tracker/ml/measures.py
+++ pitch_tracker/ml/measures.py
@@ -16,21 +16,27 @@ def class_to_frequency(class_inputs:torch.Tensor, midi_start=MIDI_START, n_class
     return output_frequencies
 
 def melody_evaluate(y_true:torch.Tensor, y_pred:torch.Tensor):
-    # y size: [n_batches, n_frames, n_classes]
+    # one-hot y size: [n_batches, n_frames, n_classes]
+    # argmax y size: [n_batches, n_frames]
     y_true, y_pred = y_true.cpu(), y_pred.cpu()
-    
-    y_true_labels = torch.argmax(y_true, dim=-1).flatten()
-    y_pred_labels = torch.argmax(y_pred, dim=-1).flatten()
 
-    ref_freq = class_to_frequency(y_true_labels)
-    est_freq = class_to_frequency(y_pred_labels)
+    if len(y_true.shape) == 3:
+        y_true = torch.argmax(y_true, dim=-1)
+        y_pred = torch.argmax(y_pred, dim=-1)
 
+    y_true = y_true.flatten()
+    y_pred = y_pred.flatten()
+
+    ref_freq = class_to_frequency(y_true)
+    est_freq = class_to_frequency(y_pred)
     time_1d = torch.arange(0, ref_freq.numel(),1)
 
     scores = evaluate(
         ref_time=time_1d.numpy(),
         ref_freq=ref_freq.numpy(),
         est_time=time_1d.numpy(),
-        est_freq=est_freq.numpy())
+        est_freq=est_freq.numpy(),
+        cent_tolerance=51
+        ),
     
     return scores 
